2c07749c806af51d51d827f1818a8eb2b83fcaf9
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-09-11 14:07:51 +0200

    add products boots styling basic

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,5 +1,18 @@
-const Product = props => {
-  return <div></div>;
+import { Card } from 'react-bootstrap';
+import classes from './Product.module.css';
+
+const Product = ({ product }) => {
+  return (
+    <div className='card-columns'>
+      <Card className={`${classes.card}`}>
+        <Card.Body>
+          <Card.Text>{product.SKU}</Card.Text>
+          <Card.Text>{product.price}</Card.Text>
+          <Card.Text>{product.name}</Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 };
 
 export default Product;
--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -1,9 +1,26 @@
 import Product from './Product';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const ProductsList = () => {
+  const [products, setProducts] = useState([]);
+  const { REACT_APP_HOST } = process.env;
+  console.log(REACT_APP_HOST);
+  useEffect(() => {
+    axios
+      .get(`${REACT_APP_HOST}api/products`)
+      .then(data => {
+        setProducts(data.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }, []);
   return (
     <>
-      <Product />
+      {products.map(product => {
+        return <Product key={product.SKU} product={product} />;
+      })}
     </>
   );
 };
