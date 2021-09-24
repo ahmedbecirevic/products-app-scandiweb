b58b744b1bfa4e3fffbfbf1c8f904de861d643db
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-09-24 18:24:39 +0200

    add checkbox component

--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,10 +1,16 @@
+import { useState } from 'react';
 import { Card } from 'react-bootstrap';
+import Checkbox from '../UI/Checkbox';
 import classes from './Product.module.css';
 
 const Product = ({ product }) => {
+  const [isChecked, setIsChecked] = useState(false);
+  const checkedHandler = () => setIsChecked(!isChecked);
+
   return (
     <div className='card-columns'>
       <Card className={`${classes.card}`}>
+        <Checkbox checked={isChecked} onChange={checkedHandler} />
         <Card.Body>
           <Card.Text>{product.SKU}</Card.Text>
           <Card.Text>{product.price}</Card.Text>
---
+++ src/components/UI/Checkbox.js
@@ -0,0 +1,14 @@
+import classes from './Checkbox.module.css';
+
+const Checkbox = ({ checked, onChange }) => {
+  return (
+    <input
+      className={classes.checkbox}
+      type='checkbox'
+      checked={checked}
+      onChange={onChange}
+    />
+  );
+};
+
+export default Checkbox;
