c1f6f5f9fbac211e48457bc7ff993e2cff88085e
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-09-28 23:33:21 +0200

    fix prod context logic

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import MainHeader from './components/MainHeader/MainHeader';
 import ProductsList from './components/Products/ProductsList';
 import Form from './components/Products/NewProduct/Form';
+import ProductsProvider from './store/ProductsProvider';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <Router>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route exact component={ProductsList} path='/' />
-          <Route exact component={Form} path='/add-product' />
-        </Switch>
-      </main>
+      <ProductsProvider>
+        <MainHeader />
+        <main>
+          <Switch>
+            <Route exact component={ProductsList} path='/' />
+            <Route exact component={Form} path='/add-product' />
+          </Switch>
+        </main>
+      </ProductsProvider>
     </Router>
   );
 }
--- src/store/ProductsProvider.js
+++ src/store/ProductsProvider.js
@@ -1,25 +1,51 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import ProductsContext from './products-context';
 
-const ProductsProvider = () => {
-  const { REACT_APP_HOST } = process.env;
+const PROD_ARRAY = [];
 
-  const deleteProductsHandler = listOfSKU => {
-    axios.delete(`${REACT_APP_HOST}api/products`, {
-      data: {
-        listOfSKU,
-      },
+const ProductsProvider = props => {
+  const { REACT_APP_HOST } = process.env;
+  const [productsToDelete, setProductsToDelete] = useState(PROD_ARRAY);
+
+  const deleteProductsHandler = () => {
+    // useEffect(() => {
+    console.log({ SKU: productsToDelete });
+    // const properFormForDeletion = {
+    //   SKU: productsContext.products.filter(),
+    // };
+    // axios.delete(`${REACT_APP_HOST}api/products`, {
+    //   data: {
+    //     properFormForDeletion,
+    //   },
+    // });
+    // }, []);
+  };
+
+  const addProductToDeleteHandler = SKU => {
+    setProductsToDelete(prevState => {
+      return [...prevState, SKU];
+    });
+  };
+
+  const removeProductFromDeleteHandler = SKU => {
+    setProductsToDelete(prevState => {
+      const newArray = prevState.filter(skuState => skuState !== SKU);
+      return newArray;
     });
   };
 
   const productsContext = {
-    products: [],
+    listProdToDelete: productsToDelete,
+    addProductToDelete: addProductToDeleteHandler,
+    removeProductFromDelete: removeProductFromDeleteHandler,
     deleteProducts: deleteProductsHandler,
   };
+
   return (
-    <ProductsContext.ProductsProvider
-      value={productsContext}
-    ></ProductsContext.ProductsProvider>
+    <ProductsContext.Provider value={productsContext}>
+      {props.children}
+    </ProductsContext.Provider>
   );
 };
 
