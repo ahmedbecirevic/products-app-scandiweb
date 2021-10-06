712548ca2c159815c6880648be9b7d9e9d28ec01
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-10-06 18:56:04 +0200

    fix footer positioning

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import classes from './Footer.module.css';
 
 const Footer = () => {
   return (
-    <div className='fixed-bottom'>
+    <div className={classes.footer}>
       <Divider variant='middle' />
       <footer className='mt-2'>
         <p className='text-center'>ScandiWeb Test Assignment</p>
